f4d494e8c275d1b6c3febf42c9f94af370eed750
kanizfatimashaikh <kanizfatimasubhanishaikh@gmail.com>
2025-05-20 22:08:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import ta
 import datetime
 import random
+import os
 
 app = Flask(__name__)
 
@@ -85,4 +86,5 @@ def report():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
